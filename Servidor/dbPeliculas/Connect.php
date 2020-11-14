8b4f8d005aaedcc4903a256fc19657465a7d23b4
jalex099 <mm18014@ues.edu.sv>
2020-11-14 07:27:00 -0600

    Definiciones

---
+++ Servidor/dbPeliculas/Connect.php
@@ -0,0 +1,24 @@
+<?php
+    require_once("config/dbConfig.php");
+    /**
+     * Clase conexion
+     */
+    class Connect
+    {
+        protected $conn;
+
+        public function Database()
+        {
+            try {
+                $this->conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME."", DB_USER, DB_PASSWORD);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->conn->exec("SET CHARACTER SET ".DB_CHARSET);
+                return $this->conn;
+
+            } catch (Exception $e) {
+                return "¡Error!: " . $e->getMessage();
+                die();
+            }
+        }
+    }
+?>
---
+++ Servidor/obtenerTodos.php
@@ -0,0 +1,7 @@
+<?php
+require_once "database/Database.php";
+
+
+
+
+?>
