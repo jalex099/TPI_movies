83221c49484cc0768457b077669e0d45a9305a96
Steve-Hndz <rh18031@ues.edu.sv>
2020-11-17 10:34:01 -0600

    update: Formato de BaseLayout

--- render/BaseLayout.php
+++ render/BaseLayout.php
@@ -1,13 +1,13 @@
 <?php
-class BaseLayout {
-
-    public static function renderHead()
+class BaseLayout //Clase Base para el diseno de la pagina
+{
+    public static function renderHead() //Render para mostrar header
     {
-        require_once "views/header.php";
+        require_once "views/header.php"; //Requerimos la vista de header
     }
 
-    public static function renderFoot()
+    public static function renderFoot() //Render para mostrar footer
     {
-        require_once "views/footer.php";
+        require_once "views/footer.php"; //Requerimos la vista de footer
     }
 }
