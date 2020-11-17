9bd278dfb2b322ebab7be62c6439e10c91da4cdd
Steve-Hndz <rh18031@ues.edu.sv>
2020-11-17 09:52:06 -0600

    update: Formato del footer actualizado

--- views/footer.php
+++ views/footer.php
@@ -1,9 +1,15 @@
-<footer>
-        <h3>USERZ - Todos los derechos reservados © 2020</h3>
+    <!-- Footer de la pagina-->
+    <footer>
+        <h3>MoviX - Todos los derechos reservados © 2020</h3>
     </footer>
+    <!-- /Footer de la pagina-->
 
-    <script src="<?=BASE_DIR;?>assets/js/main.js"></script>
+    <!-- Script con funciones propias-->
+    <script src="<?= BASE_DIR; ?>assets/js/main.js"></script>
+    <!-- Script con funciones para funcionamiento con jquery-->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous">
+    </script>
 </body>
-</html>
 
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
+</html>
