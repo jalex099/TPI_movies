d193078f5e3d30a081bc54f87532aefd8a6c968a
Steve-Hndz <rh18031@ues.edu.sv>
2020-11-17 10:20:23 -0600

    update: Estilos CSS para las vistas de la pagina

--- views/home.php
+++ views/home.php
@@ -1,46 +1,50 @@
-<main class="blog">
-<div class="blog-slider">
-  <div class="blog-slider__wrp swiper-wrapper">
-    <div class="blog-slider__item swiper-slide">
-      <div class="blog-slider__img">
-        <img src="<?=BASE_DIR;?>assets/images/img1.jpg" alt="img1">
-      </div>
-      <div class="blog-slider__content">
-        <span class="blog-slider__code">INFO 1</span>
-        <div class="blog-slider__title">Explora</div>
-        <div class="blog-slider__text">Un catálogo completo de muchas películas que ver, todas las que puedes imaginar, el maravilloso mundo del cine más cerca de ti</div>
-        <a href="<?=BASE_DIR;?>Movie/showMovies" class="blog-slider__button">Ver Catálogo</a>
-      </div>
-    </div>
-    <div class="blog-slider__item swiper-slide">
-      <div class="blog-slider__img">
-        <img src="<?=BASE_DIR;?>assets/images/img2.jpg" alt="img2">
-      </div>
-      <div class="blog-slider__content">
-        <span class="blog-slider__code">INFO 2</span>
-        <div class="blog-slider__title">Accede</div>
-        <div class="blog-slider__text">Si ya eres parte de nuestra comunidad, solo tienes que acceder para poder tener acceso al catálogo de películas</div>
-        <a href="<?=BASE_DIR;?>User/login" class="blog-slider__button">Iniciar Sesión</a>
-      </div>
-    </div>
+    <main class="picture">
+        <div class="picture-slider">
+            <div class="picture-slider__wrp swiper-wrapper">
+                <div class="picture-slider__item swiper-slide">
+                    <div class="picture-slider__img">
+                        <img src="<?=BASE_DIR;?>assets/images/img1.jpg" alt="img1">
+                    </div>
+                    <div class="picture-slider__content">
+                        <span class="picture-slider__code">INFO 1</span>
+                        <div class="picture-slider__title">Explora</div>
+                        <div class="picture-slider__text">Un catálogo completo de muchas películas que ver, todas las que
+                            puedes imaginar, el maravilloso mundo del cine más cerca de ti</div>
+                        <a href="<?=BASE_DIR;?>Movie/showMovies" class="picture-slider__button">Ver Catálogo</a>
+                    </div>
+                </div>
+                <div class="picture-slider__item swiper-slide">
+                    <div class="picture-slider__img">
+                        <img src="<?=BASE_DIR;?>assets/images/img2.jpg" alt="img2">
+                    </div>
+                    <div class="picture-slider__content">
+                        <span class="picture-slider__code">INFO 2</span>
+                        <div class="picture-slider__title">Accede</div>
+                        <div class="picture-slider__text">Si ya eres parte de nuestra comunidad, solo tienes que acceder para
+                            poder tener acceso al catálogo de películas</div>
+                        <a href="<?=BASE_DIR;?>User/login" class="picture-slider__button">Iniciar Sesión</a>
+                    </div>
+                </div>
 
-    <div class="blog-slider__item swiper-slide">
-      <div class="blog-slider__img">
-        <img src="<?=BASE_DIR;?>assets/images/img3.jpg" alt="img3">
-      </div>
-      <div class="blog-slider__content">
-        <span class="blog-slider__code">INFO 3</span>
-        <div class="blog-slider__title">Únete</div>
-        <div class="blog-slider__text">Si aún no eres miembro, ¿qué más esperas para formar parte y disfrutar de horas de entretenimiento? Hazlo ahora</div>
-        <a href="#" class="blog-slider__button">Registrarse</a>
-      </div>
-    </div>
+                <div class="picture-slider__item swiper-slide">
+                    <div class="picture-slider__img">
+                        <img src="<?=BASE_DIR;?>assets/images/img3.jpg" alt="img3">
+                    </div>
+                    <div class="picture-slider__content">
+                        <span class="picture-slider__code">INFO 3</span>
+                        <div class="picture-slider__title">Únete</div>
+                        <div class="picture-slider__text">Si aún no eres miembro, ¿qué más esperas para formar parte y
+                            disfrutar de horas de entretenimiento? Hazlo ahora</div>
+                        <a href="#" class="picture-slider__button">Registrarse</a>
+                    </div>
+                </div>
 
-  </div>
-  <div class="blog-slider__pagination"></div>
-</div>
-</main>
+            </div>
+            <div class="picture-slider__pagination"></div>
+        </div>
+    </main>
 
-<script src="<?=BASE_DIR;?>assets/js/main.js"></script>
+    <script src="<?=BASE_DIR;?>assets/js/main.js"></script>
 </body>
+
 </html>
