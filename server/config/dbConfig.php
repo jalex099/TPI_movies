6881065cf44762e092582988fd9947becc6578ae
jalex099 <mm18014@ues.edu.sv>
2020-11-18 16:07:36 -0600

    cambio de directorios

--- server/config/dbConfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-  define ('DB_HOST', 'localhost'); //Constante para nombre del host
-  define ('DB_USER', 'id14195150_tpimovies'); //Constante para nombre de usuario
-  define ('DB_PASSWORD', '$1ww(D#4jq%Y*NFH'); //Constante para contrenia
-  define ('DB_NAME', 'id14195150_db_peliculas'); //Constante para nombre de la base de datos
-  define ('DB_CHARSET', 'utf8'); //Constante para el tipo de cotejamiento
-?>
